0147fc3b0ae1e5580eef7793c6baf19dc35ac617
Dagmara Grabowska <dagmarag0@gmail.com>
2023-08-16 23:12:25 +0200

    Add classes and methods

--- script.js
+++ script.js
@@ -3,3 +3,35 @@ const ctx = canvas.getContext("2d")
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+class Particle {
+    constructor() {
+
+    }
+    draw() {
+
+    }
+    update() {
+
+    }
+    reset() {
+        
+    }
+}
+
+class Effect {
+    constructor() {
+
+    }
+    createParticles() {
+
+    }
+    handleParticles() {
+
+    }
+    connectParticles() {
+
+    }
+    resize() {
+        
+    }
+}
